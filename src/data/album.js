92d4037880bfbefcc9144cc0a9c0b96398a8bb57
Juan <gonzalezgarciajuandelacruz4@gmail.com>
2025-12-14 02:56:03 -0300

    Corrijo rutas de música en album.js para que coincidan con archivos reales

--- src/data/album.js
+++ src/data/album.js
@@ -1,65 +1,63 @@
-// src/data/album.js
-
 export const album = [
   {
     id: "1",
     foto: { src: "/images/foto1.jpg", alt: "Recuerdo 1" },
-    musica: { src: "/music/foto1_nombre.mp3", title: "miamor" }
+    musica: { src: "/music/foto1_miamor.mp3", title: "miamor" }
   },
   {
     id: "2",
     foto: { src: "/images/foto2.jpg", alt: "Recuerdo 2" },
-    musica: { src: "/music/foto2_nombre.mp3", title: "micorazon" }
+    musica: { src: "/music/foto2_micorazon.mp3", title: "micorazon" }
   },
   {
     id: "3",
     foto: { src: "/images/foto3.jpg", alt: "Recuerdo 3" },
-    musica: { src: "/music/foto3_nombre.mp3", title: "mifuturo" }
+    musica: { src: "/music/foto3_mifuturo.mp3", title: "mifuturo" }
   },
   {
     id: "4",
     foto: { src: "/images/foto4.jpg", alt: "Recuerdo 4" },
-    musica: { src: "/music/foto4_nombre.mp3", title: "miluz" }
+    musica: { src: "/music/foto4_miluz.mp3", title: "miluz" }
   },
   {
     id: "5",
     foto: { src: "/images/foto5.jpg", alt: "Recuerdo 5" },
-    musica: { src: "/music/foto5_nombre.mp3", title: "mimundo" }
+    musica: { src: "/music/foto5_mimundo.mp3", title: "mimundo" }
   },
   {
     id: "6",
     foto: { src: "/images/foto6.jpg", alt: "Recuerdo 6" },
-    musica: { src: "/music/foto6_nombre.mp3", title: "minina" }
+    musica: { src: "/music/foto6_minina.mp3", title: "minina" }
   },
   {
     id: "7",
     foto: { src: "/images/foto7.jpg", alt: "Recuerdo 7" },
-    musica: { src: "/music/foto7_nombre.mp3", title: "minovia" }
+    musica: { src: "/music/foto7_minovia.mp3", title: "minovia" }
   },
   {
     id: "8",
     foto: { src: "/images/foto8.jpg", alt: "Recuerdo 8" },
-    musica: { src: "/music/foto8_nombre.mp3", title: "mireina" }
+    musica: { src: "/music/foto8_mireina.mp3", title: "mireina" }
   },
   {
     id: "9",
     foto: { src: "/images/foto9.jpg", alt: "Recuerdo 9" },
-    musica: { src: "/music/foto9_nombre.mp3", title: "misueno" }
+    musica: { src: "/music/foto9_misueno.mp3", title: "misueno" }
   },
   {
     id: "10",
     foto: { src: "/images/foto10.jpg", alt: "Recuerdo 10" },
-    musica: { src: "/music/foto10_nombre.mp3", title: "miuniverso" }
+    musica: { src: "/music/foto10_miuniverso.mp3", title: "miuniverso" }
   },
   {
     id: "11",
     foto: { src: "/images/foto11.jpg", alt: "Recuerdo 11" },
-    musica: { src: "/music/foto11_nombre.mp3", title: "mivida" }
+    musica: { src: "/music/foto11_mivida.mp3", title: "mivida" }
   },
   {
     id: "12",
     foto: { src: "/images/foto12.jpg", alt: "Recuerdo 12" },
-    musica: { src: "/music/foto12_nombre.mp3", title: "pidemelaluna" }
+    musica: { src: "/music/foto12_pidemelaluna.mp3", title: "pidemelaluna" }
   },
   {
     id: "13",
@@ -69,32 +67,32 @@ export const album = [
   {
     id: "14",
     foto: { src: "/images/foto14.jpg", alt: "Recuerdo 14" },
-    musica: { src: "/music/foto14_nombre.mp3", title: "eselocosoyyo" }
+    musica: { src: "/music/foto14_eselocosoyyo.mp3", title: "eselocosoyyo" }
   },
   {
     id: "15",
     foto: { src: "/images/foto15.jpg", alt: "Recuerdo 15" },
-    musica: { src: "/music/foto15_nombre.mp3", title: "cristina" }
+    musica: { src: "/music/foto15_Cristina.mp3", title: "Cristina" }
   },
   {
     id: "16",
     foto: { src: "/images/foto16.jpg", alt: "Recuerdo 16" },
-    musica: { src: "/music/foto16_nombre.mp3", title: "amorsiempre" }
+    musica: { src: "/music/foto16_amorsiempre.mp3", title: "amorsiempre" }
   },
   {
     id: "17",
     foto: { src: "/images/foto17.jpg", alt: "Recuerdo 17" },
-    musica: { src: "/music/foto17_nombre.mp3", title: "angel" }
+    musica: { src: "/music/foto17_angel.mp3", title: "angel" }
   },
   {
     id: "18",
     foto: { src: "/images/foto18.jpg", alt: "Recuerdo 18" },
-    musica: { src: "/music/foto18_nombre.mp3", title: "aliadodeltiempo" }
+    musica: { src: "/music/foto18_aliadodeltiempo.mp3", title: "aliadodeltiempo" }
   },
   {
     id: "19",
     foto: { src: "/images/foto19.jpg", alt: "Recuerdo 19" },
-    musica: { src: "/music/foto19_nombre.mp3", title: "amorsincero" }
+    musica: { src: "/music/foto19_Amorsincero.mp3", title: "Amor sincero" }
   }
 ];
 
